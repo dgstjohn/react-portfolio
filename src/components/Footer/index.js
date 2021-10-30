38a432b500b426163e53081218f8ee9bd7b14aee
dgstjohn <dgstjohn@gmail.com>
2021-10-30 01:46:07 -0400

    added linked logos to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,12 @@
 import React from "react";
+import Twitter from "../../assets/twitter-logo.png";
+import LinkedIn from "../../assets/linkedin-logo.png";
+import GitHub from "../../assets/github-logo.png";
 
 function Footer() {
     return (
-        <footer>
+        <footer className="footer">
+            <div className="logos"><a href="https://github.com/dgstjohn"><img src={GitHub} /></a><a href="https://www.linkedin.com/in/donstjohn1/"><img src={LinkedIn} /></a><a href="https://twitter.com/donstjohn"><img src={Twitter} /></a></div>
             <div>&copy; Copyright 2021 Donald St. John</div>
         </footer>
     )
