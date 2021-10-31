1b114946d3ac0e3c0930aaecccd25c8e006dbdba
dgstjohn <dgstjohn@gmail.com>
2021-10-31 19:43:56 -0400

    trying change in About route to fix deploy bug

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ function App() {
         <div>
           <Switch>
             <Route exact path="/" component={About} />
-            <Route exact path="/about" component={About} />
             <Route exact path="/portfolio" component={Portfolio} />
             <Route exact path="/resume" component={Resume} />
             <Route exact path="/contact" component={Contact} />
